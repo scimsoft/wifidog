fc49bb80503d3a75f79712a50ff24cd7814b9454
Gerrit <gerrit.sevilla@gmail.com>
2019-06-01 21:48:55 +0200

    login page mejoras

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
 
 			<p>My name: {{Auth::user()->name}}</p>
 			<p>My Email: {{Auth::user()->email}}</p>
-			<img alt="{{Auth::user()->name}}" src="{{Auth::user()->image}}"/>
+
                
                     @if (!empty($msg))
                         <div class="alert alert-info" role="alert">
